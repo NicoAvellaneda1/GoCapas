a497fc4ca9488b58b23395da02e5520394cc749d
“NicoAvellaneda1” <rodolfo.avellaneda@mercadolibre.com>
2021-12-30 10:33:18 -0300

    Proyecto 4

--- cmd/server/handler/users.go
+++ cmd/server/handler/users.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/NicoAvellaneda1/GoCapas/internal/users"
+	response "github.com/NicoAvellaneda1/GoCapas/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
@@ -29,53 +30,63 @@ func NewUser(u users.Service) *User {
 	}
 }
 
+// GetUsers godoc
+// @Summary Lista de usuarios
+// @Description get users
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// Success 200 {object} web.
+// @Router /users [get]
 func (u *User) GetAll() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
+		//comparo el token desde el archivo .env
 		if token != os.Getenv("TOKEN") {
-			c.JSON(401, gin.H{
-				"error": "token invalido",
-			})
+			c.JSON(401, response.NewResponse(401, nil, "token invalido"))
 			return
 		}
 
 		usuarios, err := u.service.GetAll()
 		if err != nil {
-			c.JSON(401, gin.H{
-				"error": err.Error(),
-			})
+			c.JSON(404, response.NewResponse(404, nil, err.Error()))
 			return
 		}
-		c.JSON(200, usuarios)
+		c.JSON(200, response.NewResponse(200, usuarios, ""))
 	}
 }
 
+// StoreUsers godoc
+// @Summary Store users
+// @Description store users
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param token header string true "token"
+// @Param user body request true "User to store"
+// Success 200 {object} web.Response
+// @Router /users [post]
 func (u *User) Store() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			c.JSON(404, gin.H{
-				"error": "token ivalido",
-			})
+			c.JSON(401, response.NewResponse(401, nil, "token invalido"))
 			return
 		}
 
 		var req request
 		if err := c.Bind(&req); err != nil {
-			c.JSON(400, gin.H{
-				"error": err.Error(),
-			})
+			c.JSON(400, response.NewResponse(400, nil, err.Error()))
 			return
 		}
 
 		usuarios, err := u.service.Store(req.Nombre, req.Apellido, req.Email, req.Edad, req.Altura, req.Activo, req.FechaCreacion)
 		if err != nil {
-			c.JSON(404, gin.H{
-				"error": err.Error(),
-			})
+			c.JSON(404, response.NewResponse(404, nil, err.Error()))
 			return
 		}
-		c.JSON(200, usuarios)
+		c.JSON(200, response.NewResponse(200, usuarios, ""))
 	}
 }
 
@@ -83,61 +94,55 @@ func (u *User) Update() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			c.JSON(401, gin.H{
-				"error": "token invalido",
-			})
+			c.JSON(401, response.NewResponse(401, nil, "token invalido"))
 			return
 		}
 
 		//convierto a int el parametro recibido como string
 		id, err := strconv.ParseInt(c.Param("id"), 10, 64)
 		if err != nil {
-			c.JSON(400, gin.H{
-				"error": "ID invalido",
-			})
+			c.JSON(400, response.NewResponse(400, nil, err.Error()))
 			return
 		}
 
 		var req request
 		if err := c.ShouldBindJSON(&req); err != nil {
-			c.JSON(404, gin.H{
-				"error": err.Error(),
-			})
+			c.JSON(404, response.NewResponse(404, nil, err.Error()))
 			return
 		}
 
 		//realizo las validaciones
 		if req.Nombre == "" {
-			c.JSON(400, gin.H{"error": "el nombre es requerido"})
+			c.JSON(400, response.NewResponse(400, nil, "el nombre es requerido"))
 			return
 		}
 		if req.Apellido == "" {
-			c.JSON(400, gin.H{"error": "el apellido es requerido"})
+			c.JSON(400, response.NewResponse(400, nil, "el apellido es requerido"))
 			return
 		}
 		if req.Email == "" {
-			c.JSON(400, gin.H{"error": "el email es requerido"})
+			c.JSON(400, response.NewResponse(400, nil, "el email es requerido"))
 			return
 		}
 		if req.Edad == 0 {
-			c.JSON(400, gin.H{"error": "la edad es requerida"})
+			c.JSON(400, response.NewResponse(400, nil, "la edad es requerida"))
 			return
 		}
 		if req.Altura == 0 {
-			c.JSON(400, gin.H{"error": "la altura es requerida"})
+			c.JSON(400, response.NewResponse(400, nil, "la altura es requerida"))
 			return
 		}
 		if req.FechaCreacion == "" {
-			c.JSON(400, gin.H{"error": "el fechaCreacion es requerido"})
+			c.JSON(400, response.NewResponse(400, nil, "la fechaCreacion es requerida"))
 			return
 		}
 
 		us, err := u.service.Update(int(id), req.Nombre, req.Apellido, req.Email, req.Edad, req.Altura, req.Activo, req.FechaCreacion)
 		if err != nil {
-			c.JSON(404, gin.H{"error": err.Error()})
+			c.JSON(404, response.NewResponse(404, nil, err.Error()))
 			return
 		}
-		c.JSON(200, us)
+		c.JSON(200, response.NewResponse(200, us, ""))
 	}
 }
 
@@ -145,31 +150,31 @@ func (u *User) UpdateName() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
-			c.JSON(401, gin.H{"error": "token invalido"})
+			c.JSON(401, response.NewResponse(401, nil, "token invalido"))
 			return
 		}
 
 		id, err := strconv.ParseInt(c.Param("id"), 10, 64)
 		if err != nil {
-			c.JSON(401, gin.H{"error": "ID invalido"})
+			c.JSON(401, response.NewResponse(401, nil, "ID invalido"))
 			return
 		}
 
 		var req request
 		if err := c.ShouldBindJSON(&req); err != nil {
-			c.JSON(404, gin.H{"error": err.Error()})
+			c.JSON(404, response.NewResponse(404, nil, err.Error()))
 		}
 
 		if req.Nombre == "" {
-			c.JSON(400, gin.H{"error": "el nombre es requerido"})
+			c.JSON(400, response.NewResponse(400, nil, "el nombre es requerido"))
 		}
 
 		us, err := u.service.UpdateName(int(id), req.Nombre)
 		if err != nil {
-			c.JSON(404, gin.H{"error": err.Error()})
+			c.JSON(404, response.NewResponse(404, nil, err.Error()))
 			return
 		}
-		c.JSON(200, us)
+		c.JSON(200, response.NewResponse(200, us, ""))
 	}
 }
 
@@ -177,19 +182,20 @@ func (u *User) Delete() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
 		if token != os.Getenv("TOKEN") {
+			//forma anterior de devolver el error
 			c.JSON(401, gin.H{"error": "token invalido"})
 			return
 		}
 
 		id, err := strconv.ParseInt(c.Param("id"), 10, 64)
 		if err != nil {
-			c.JSON(401, gin.H{"error": "ID invalido"})
+			c.JSON(401, response.NewResponse(401, nil, "ID invalido"))
 			return
 		}
 
 		err = u.service.Delete(int(id))
 		if err != nil {
-			c.JSON(404, gin.H{"error": err.Error()})
+			c.JSON(404, response.NewResponse(404, nil, err.Error()))
 			return
 		}
 		c.JSON(200, gin.H{
